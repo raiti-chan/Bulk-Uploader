7af5950e221e9cdd138a2fdfd3122c401b814723
らいちちゃん <rusuke@t-net.ne.jp>
2024-12-05 17:58:14 +0900

    fix: syntax error

--- Packages/net.raitichan.avatar.bulk-uploader/Runtime/ScriptableObject/TargetScenesDefine.cs
+++ Packages/net.raitichan.avatar.bulk-uploader/Runtime/ScriptableObject/TargetScenesDefine.cs
@@ -19,6 +19,8 @@ namespace net.raitichan.avatar.bulk_uploader.Runtime.ScriptableObject {
 #if UNITY_EDITOR
 		public SceneAsset? Scene;
 #endif
+	}
+
 	[Serializable]
 	public class AvatarDefine {
 		public bool Enable;
